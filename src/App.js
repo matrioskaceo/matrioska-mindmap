ce83e4b7df8531093f10e8b4ad9580595bd571cf
matrioskaceo <victor@matrioska.io>
2025-03-04 23:26:46 +0100

    Fixed mindmap rendering and integrated Google Sheets data

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Mindmap from "./mindmap";
+
+function App() {
+  console.log("✅ App.js está renderizando correctamente");
+
+  return (
+    <div>
+      <h1>Mindmap Matrioska</h1>
+      <Mindmap />
+    </div>
+  );
+}
+
+export default App;
+
